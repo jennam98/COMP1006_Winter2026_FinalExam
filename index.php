a62f4080e9a44b7a3bc420da369f7e723ba83d37
Jenna Moss <jenna.moss@hotmail.com>
2026-04-15 14:19:25 -0400

    adding html structure to index

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Gallery</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Image Gallery</h1>
+    <a class="btn btn-primary" href="create.php">Upload New Image</a>
+   
+    </div>
+</body>
+</html>
